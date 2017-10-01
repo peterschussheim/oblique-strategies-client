f7779b208ecee2ec7dc8f59fb8c7454ae4761773
Peter Schussheim <peter@schussheim.com>
2017-10-01 17:01:11 -0400

    change styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react'
 import './App.css'
 import { randomizer } from './util'
+import Footer from './Footer'
+
 import cards from './Cards.json'
 
 class App extends Component {
   state = {
-    currentCard: randomizer(cards)
+    currentCard: randomizer(cards),
+    isRefreshEnabled: false,
+    refreshSpeed: 3,
+    showEdition: false
   }
   render() {
+    const { currentCard, showEdition } = this.state
     return (
-      <div>
-        <header className="tc ph4">
-          <h1 className="f3 f2-m f1-l fw2 black-90 mv3">
-            {this.state.currentCard.content}
-          </h1>
-          <h2 className="f5 f4-m f3-l fw2 black-50 mt0 lh-copy">
-            <label>Edition: </label>
-            {this.state.currentCard.edition}
-          </h2>
-        </header>
+      <div className="ma2">
+        <div className="mw7 center pa2 mt6 mb6 bg-gray">
+          <header className="mw7-ns ml1 mr1 mt3 mb3 center">
+            <h1 className="tc f3 f2-m f1-l fw2 mv3 mt0 black">
+              {currentCard.content}
+            </h1>
+          </header>
+          {showEdition ? (
+            <span className=" fw2 silver mt0 lh-copy">
+              <label>Edition: </label>
+              {currentCard.edition}
+            </span>
+          ) : null}
+        </div>
+        <Footer />
       </div>
     )
   }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => (
+  <footer className="pv4 ph3 ph5-m ph6-l bg-white mid-gray">
+    <small className="f6 db tc">
+      <b className="ttu">
+        <a href="https://peterschussheim.com/">Peter Schussheim</a>
+      </b>
+    </small>
+  </footer>
+)
