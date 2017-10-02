1766aa72778a59f42e02697d22169d7497b25db2
Peter Schussheim <peter@schussheim.com>
2017-10-02 15:04:32 -0400

    add button to refresh strategy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import { randomizer } from './util'
 import Footer from './Footer'
-
+import Button from './Button'
 import cards from './Cards.json'
 
 class App extends Component {
@@ -12,10 +12,21 @@ class App extends Component {
     refreshSpeed: 3,
     showEdition: false
   }
+
+  handleRefresh = event => {
+    event.preventDefault()
+    this.setState(prevState => ({
+      currentCard: randomizer(cards)
+    }))
+    console.log(this.state)
+  }
+
   render() {
     const { currentCard, showEdition } = this.state
     return (
       <div className="ma2">
+        <Button onClick={this.handleRefresh}>Load another strategy...</Button>
+        <Button onClick={this.handleRefresh}>Show Edition?</Button>
         <div className="mw7 center pa2 mt6 mb6 bg-gray">
           <header className="mw7-ns ml1 mr1 mt3 mb3 center">
             <h1 className="tc f3 f2-m f1-l fw2 mv3 mt0 black">
---
+++ src/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import classnames from 'classnames'
+
+const Button = props => (
+  <button type="button" className="f7 dim ph3 pv2 mb2 dib blue" {...props} />
+)
+
+export default Button
